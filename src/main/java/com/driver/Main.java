f02b53f77f35d486f6f1584d5e59150a1c1ca9be
Likhitha <veshalalikhitha@gmail.com>
2024-01-22 17:54:28 +0530

    Resolved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,6 @@
 package com.driver;
 
+
 public class Main {
   public static void main(String args[]){
       A obja = new A();
@@ -7,16 +8,7 @@ public class Main {
       System.out.println(objb.meth());
   }
 }
-class A{
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    public String meth(){
-        return "Method is overridden in Extended class B";
-    }
 
-}
+
 
 
