ef549aebf23934408a0492147599ca24fe708c90
Likhitha <veshalalikhitha@gmail.com>
2024-01-22 17:44:41 +0530

    Changed code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,21 @@ package com.driver;
 
 public class Main {
   public static void main(String args[]){
+      A obja = new A();
       B objb = new B();
       System.out.println(objb.meth());
   }
 }
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    public String meth(){
+        return "Method is overridden in Extended class B";
+    }
+
+}
+
+
